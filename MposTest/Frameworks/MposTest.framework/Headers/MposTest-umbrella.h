1d3c79e6573fa83813dfcabd8f2fe71a4ad253ca
Alexandre Soler <alexandresoler@dev14.lbg.office.lyra>
2016-11-22 10:37:16 +0100

    up

---
+++ MposTest/Frameworks/MposTest.framework/Headers/MposTest-umbrella.h
@@ -0,0 +1,10 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+#import "MposTest.h"
+#import "MposTest2.h"
+
+FOUNDATION_EXPORT double MposTestVersionNumber;
+FOUNDATION_EXPORT const unsigned char MposTestVersionString[];
+
---
+++ MposTest/Frameworks/MposTest.framework/Headers/MposTest2.h
@@ -0,0 +1,23 @@
+//
+//  MposTest2.h
+//  Pods
+//
+//  Created by Alexandre Soler on 21/11/2016.
+//
+//
+
+#ifndef MposTest2_h
+#define MposTest2_h
+
+@interface MposTest2 : NSObject
+{
+    id moteur;
+    NSArray * passagers;
+}
+
+- (void) initBlabla2; // Voici la déclaration de - init
+
+@end
+
+
+#endif /* MposTest2_h */
